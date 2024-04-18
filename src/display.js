d84ead2c07f5d88309e01013267f407ead070c77
Grant Oxer <grant.oxer@gmail.com>
2024-04-18 12:49:57 -0700

    got copy button to appear when displaying results

--- src/display.js
+++ src/display.js
@@ -147,6 +147,7 @@ function results(cb) {
 
     jQuery(".results_phrase_key").hide().clone().appendTo(".results");
     jQuery(".results_phrase_value").hide().clone().appendTo(".results");
+    jQuery(".copy_button_div").hide().clone().appendTo(".results");
     jQuery(".results").append("<br clear=\"all\" />");
 
     jQuery(".results_num_possible_key").hide().clone().appendTo(".results");
@@ -158,6 +159,8 @@ function results(cb) {
         return (jQuery(".results .results_phrase_key").fadeIn(400).promise());
     }).then(function () {
         return (jQuery(".results .results_phrase_value").fadeIn(400).promise());
+    }).then(function () {
+        return (jQuery(".results .copy_button_div").fadeIn(400).promise());
     }).then(function () {
         return (jQuery(".results .results_num_possible_key").fadeIn(300).promise());
     }).then(function () {
